c561a19258327c92d0f21a7e5c5704c50f2da48e
Leonardo Oliveira Rosa <107898142+LeonardoOliveiraRosaDev@users.noreply.github.com>
2022-08-12 20:06:20 -0300

    12082022

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+/* Reestruturacao do arquivo index.js para que comporte a store
+indicando, nele, o provedor (provider) de conteudo a apliação.
+*/
+
+import React,{Component} from "react";
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './App'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+// Realizar o novos importes para que a store seja criada e possa 
+// servir como provedor de conteudo da aplicação - através do 
+// armazenamento dos state.
+
+import {render} from 'react-dom';
+import { legacy_createStore as createStore} from "redux";
+import {Provider} from 'react-redux';
+
+// importante a constante que possui como valor atribuido os state
+// do reducer
+
+import todoApp from "./reducers/reducers";
+
+// Definir a propriedade que será responsável por receber o valor a
+// criação da store e, também, o armazenamento do states.
+let criarStore = createStore(todoApp);
+
+let rootElement = document.getElementById('root');
+
+// Implementando o provider(provedor de conteudo)
+render(
+  <Provider proverStore = {criarStore}>
     <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  </Provider>,
+  rootElement
+)
