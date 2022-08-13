0e11ff5f6e99bfff735925b5ccea57806af15786
Leonardo Oliveira Rosa <107898142+LeonardoOliveiraRosaDev@users.noreply.github.com>
2022-08-12 21:19:36 -0300

    12082022-21:19

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 // Importar recursos nescessários os componentes do projeto
 import AddTodo from './componentes/AddTodo';
 import TodoList from './componentes/TodoList';
+// importa o css
 import './App.css';
+// importar o recurso para definir o componente da Classe
 import React, {Component} from 'react';
+// importar os novos recursos para o componente principal
+import { connect } from 'react-redux';
+import {addTodo} from './actions/actions';
 
+// Definir o componente classe
+class App extends Component{
+  // chamar o metodo render()
+  render(){
+    // construir a view
+    return(
+      <div className="App">
+        <div className="App-header">
+          <AddTodo />
+          <TodoList />
+        </div>
+
+      </div>
+    )
+  }
+}
+
+export default App;
 
 
