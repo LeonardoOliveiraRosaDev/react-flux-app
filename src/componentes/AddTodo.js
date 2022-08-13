3cf5f159d16266d0de16a03a4a8a6d7709734961
Leonardo Oliveira Rosa <107898142+LeonardoOliveiraRosaDev@users.noreply.github.com>
2022-08-12 21:43:06 -0300

    12082022-21:39

---
+++ src/componentes/AddTodo.js
@@ -0,0 +1,10 @@
+// importar somente os recurso para criar nosso componente
+// de classe
+import React, {Component} from "react";
+
+// Definir o componente de classe
+class AddTodo extends Component {
+    
+}
+
+export default AddTodo;
